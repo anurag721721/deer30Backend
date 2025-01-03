30f3e5943668a98dfea36d8a14b5199e4fb84ea5
anurag721721 <anurag.yadav@aayaninfotech.com>
2025-01-03 17:59:00 +0530

    product api

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,9 +1,11 @@
 const express=require('express');
-const { getAllProduct, createProduct } = require('../controllers/productController');
+const { getAllProduct, createProduct, getLatestProduct ,getProductDetailsbyId} = require('../controllers/productController');
 const router = express.Router();
 
 router.post('/create-product', createProduct);
 router.get('/get-all-product', getAllProduct);
+router.get('/get-latest-product', getLatestProduct);
+router.get('/get-product-details/:id', getProductDetailsbyId);
 
 
 module.exports = router;
